c415c2e23cbc36fbf1445dedf8b36aa5b4456168
matheus rodrigues monte <matheusr.monte2004@gmail.com>
2023-10-11 21:50:21 -0300

    atualização

--- screens/cursos/Cursos.js
+++ screens/cursos/Cursos.js
@@ -1,9 +1,32 @@
-import React from 'react'
-import { Button, Text } from 'react-native-paper'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import React, { useEffect, useState } from 'react'
+import { ScrollView } from 'react-native'
+import { Button, Card, Text } from 'react-native-paper'
 
 const Cursos = ({navigation}) => {
+
+  const [cursos, setCursos] = useState([])
+
+  useEffect(() =>{
+    AsyncStorage.getItem('cursos').then(resultado =>{
+      
+      resultado = JSON.parse(resultado) || []
+
+      console.log(resultado)
+      setCursos(resultado)
+    })
+  },[])
+
   return (
-    <>
+    <ScrollView>
+      {cursos.map(item=>(
+       <Card style={{margin:10, padding:15}}> 
+        <Text>{item.nome}</Text>
+        <Text>{item.duracao} </Text>
+        <Text>{item.modalidade}</Text>
+       </Card>
+      ))}
+
       <Text style={{padding:10,alignSelf:'center'}}>encaminhando para o formulário </Text>
       <Button style={{marginHorizontal:100}}
       icon='plus' 
@@ -12,7 +35,7 @@ const Cursos = ({navigation}) => {
       >
         Novo
         </Button>    
-    </>
+    </ScrollView>
   )
 }
 
--- screens/cursos/CursosForm.js
+++ screens/cursos/CursosForm.js
@@ -1,37 +1,52 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import React, { useState } from 'react'
 import { ScrollView } from 'react-native'
 import { Button, Text, TextInput } from 'react-native-paper'
 
-const CursosForm = () => {
-  
+const CursosForm = ({ navigation }) => {
+
   const [dados, setDados] = useState({})
 
-  function handleChange(valor,campo){
-    setDados({...dados,[campo]:valor})
+  function handleChange(valor, campo) {
+    setDados({ ...dados, [campo]: valor })
   }
 
-  function salvar(){
-    
-    
-    console.log(dados)
-  }
   
-  function handleChange(valor){
-    console.warn(valor)
+  function salvar() {
+
+    AsyncStorage.getItem('cursos').then(resultado => {
+
+      const cursos = JSON.parse(resultado) || []
+
+      cursos.push(dados)
+      console.log(dados)
+
+
+      AsyncStorage.setItem('cursos', JSON.stringify(cursos)) //
+
+      navigation.goBack()
+
+    })
+
+
+    
+
   }
+
+
 
   return (
     <>
       <ScrollView style={{ margin: 15 }}>
-        <Text style={{padding: 10, alignSelf: 'center'}}>Formulário de cursos</Text>
+        <Text style={{ padding: 10, alignSelf: 'center' }}>Formulário de cursos</Text>
 
         <TextInput
           style={{ margin: 10 }}
           mode='outlined'
           label='Nome'
           value={dados.nome}
-          onChangeText={(valor) =>handleChange(valor,'nome')} 
-          />
+          onChangeText={(valor) => handleChange(valor, 'nome')}
+        />
 
         <TextInput
           style={{ margin: 10 }}
@@ -39,18 +54,18 @@ const CursosForm = () => {
           label='Duração'
           value={dados.duracao}
           keyboardType='decimal-pad'
-          onChangeText={(valor) => handleChange(valor,'duracao')} 
-          />
+          onChangeText={(valor) => handleChange(valor, 'duracao')}
+        />
 
         <TextInput
           style={{ margin: 10 }}
           mode='outlined'
           label='Modalidade'
           value={dados.modalidade}
-          onChangeText={(valor) =>handleChange(valor,'modalidade')} 
-          />
+          onChangeText={(valor) => handleChange(valor, 'modalidade')}
+        />
 
-          <Button onPress={salvar}>Salvar </Button>
+        <Button onPress={salvar}>Salvar </Button>
 
       </ScrollView>
     </>
